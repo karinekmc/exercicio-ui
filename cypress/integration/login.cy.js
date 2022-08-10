07158cb8b2b55abdc3ba97308e883832de7a9af7
Karine Maciel <karine.kmc@hotmail.com>
2022-08-09 21:08:02 -0300

    commit exercicio

---
+++ cypress/integration/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+context ( 'Funcionalidade Login' , () =>{
+
+    it('Deve fazer login com sucesso' , () =>{
+
+    })
+
+    it('Deve exibir mensagem de erro ao inserir usuario ou senha invalida' , () =>{
+        
+    })
+})
